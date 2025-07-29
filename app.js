0ed6c31b250c09f54c54e81f54b197b96441b19e
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2025-07-29 20:07:23 -0300

    first commit secret friend challenge

---
+++ app.js
@@ -0,0 +1,39 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+// Fucionalidades:
+// Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo
+//  agregarán a una lista visible al hacer clic en "Adicionar".
+
+// Validar entrada: Si el campo de texto está vacío, el programa mostrará
+//  una alerta pidiendo un nombre válido.
+
+// Visualizar la lista: Los nombres ingresados aparecerán en una lista
+//  debajo del campo de entrada.
+
+// Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará
+//  aleatoriamente un nombre de la lista y se mostrará en la página.
+
+const listaAmigos = document.getElementById('listaAmigos');
+const amigo = document.getElementById('amigo');
+const amigosAgregados = [];
+
+const resultado = document.getElementById('resultado');
+
+function agregarAmigo() {
+
+    if (amigo.value) {
+        amigosAgregados.push(amigo.value + " ");
+        listaAmigos.innerHTML = amigosAgregados;
+
+    } else {
+        alert("amigo not found.");
+    }
+}
+
+function sortearAmigo() {
+    const numeroRandomLimitado = Math.floor(Math.random() * amigosAgregados.length); 
+    console.log(numeroRandomLimitado)
+    const amigoRandom = amigosAgregados[numeroRandomLimitado];
+    resultado.innerHTML = amigoRandom;
+}
