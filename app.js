eda89ce126d6f10c53f93a90df3419ee1587e8bf
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2025-07-29 20:55:26 -0300

    added desafio extra

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-
 // Fucionalidades:
 // Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo
 //  agregarán a una lista visible al hacer clic en "Adicionar".
@@ -14,26 +13,42 @@
 // Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará
 //  aleatoriamente un nombre de la lista y se mostrará en la página.
 
+
 const listaAmigos = document.getElementById('listaAmigos');
 const amigo = document.getElementById('amigo');
 const amigosAgregados = [];
 
 const resultado = document.getElementById('resultado');
 
+function limpiarValor() {
+    amigo.value = ""; 
+}
+
+function actualizarLista() {
+    listaAmigos.innerHTML = ""; // clean list
+
+    for (let i = 0; i < amigosAgregados.length; i++) {
+        let nuevoAmigoFormateado = document.createElement('li');
+        nuevoAmigoFormateado.textContent = amigosAgregados[i];
+        listaAmigos.appendChild(nuevoAmigoFormateado);
+    }
+}
+
 function agregarAmigo() {
 
     if (amigo.value) {
-        amigosAgregados.push(amigo.value + " ");
-        listaAmigos.innerHTML = amigosAgregados;
+        amigosAgregados.push(amigo.value);
+        actualizarLista();
 
     } else {
         alert("Ingresar un nombre válido.");
     }
+
+    limpiarValor(); // clean value
 }
 
 function sortearAmigo() {
     const numeroRandomLimitado = Math.floor(Math.random() * amigosAgregados.length); 
-    console.log(numeroRandomLimitado)
     const amigoRandom = amigosAgregados[numeroRandomLimitado];
     resultado.innerHTML = amigoRandom;
 }
